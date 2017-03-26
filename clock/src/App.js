18c5e1132034d2cb1c5c8bde5c36a0f66b3ac2b3
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-03-25 21:16:06 -0600

    added clock face, modified time display

--- clock/src/App.js
+++ clock/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App"> 
-        <NavBar title={'clock'}/>
+        <NavBar title={'The Face of Time'}/>
         <Clock/>
       </div>
     );
--- clock/src/Clock.js
+++ clock/src/Clock.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'; 
 import './Clock.css';
+import {Icon} from 'react-fa';
 
 class Clock extends Component {
     constructor(props) {
@@ -21,11 +22,32 @@ class Clock extends Component {
     } 
    
   render() {
+        let hour = this.state.date.getHours();
+        let min = this.state.date.getMinutes();
+        let sec = this.state.date.getSeconds();
+        let rotate = '270';
+         
+        if (sec < 15) { rotate = '0';
+        //} else if (sec < 23) { rotate = '90';
+        } else if (sec < 30) { rotate = '90';
+        //} else if (sec < 38) { rotate = '180';
+        } else if (sec < 45) { rotate = '180';
+        //} else if (sec < 53) { rotate = '270';
+        } 
+
+        sec = ("0" + sec).slice(-2);
+        hour = ("0" + hour).slice(-2);
+
+      let cardinal = 'AM';
+       if (hour >= 12) { cardinal = 'PM'; } 
+
     return (
-      <div className="clock"> 
-        {this.state.date.toLocaleTimeString()}
-      </div>
-      
+      <div className="clock">
+       
+        <div class="clockBox">
+        <Icon rotate={rotate} size='3x' name="clock-o" /><br/> {hour}:{min}:{sec} {cardinal}
+        </div>
+      </div>  
     );
   }
 }
